6e1703e739c5ab4c43e65f0ab407c234a146f513
Shamil <shamil.300085@gmail.com>
2024-06-25 16:40:51 -0700

    Week 7 Task is complete

---
+++ selenium_basics/components/base.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Base:
+    BASE_VAR = "Base Var"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click(self, locator):
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.element_to_be_clickable(locator))
+        element.click()
